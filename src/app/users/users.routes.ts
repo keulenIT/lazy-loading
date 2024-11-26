c4c880e7bcf48169a7d3932adb6e9ad363bc95e8
Mathias Keulen <mathias.keulen@external.engie.com>
2024-11-26 19:34:06 +0100

    feat(lazy-loading): provide service for routes

--- src/app/users/users.routes.ts
+++ src/app/users/users.routes.ts
@@ -5,24 +5,31 @@ import {
   NewTaskComponent,
   canLeaveEditPage,
 } from '../tasks/new-task/new-task.component';
+import { TasksService } from '../tasks/tasks.service';
 
 export const routes: Routes = [
   {
     path: '',
-    redirectTo: 'tasks',
-    pathMatch: 'full',
-  },
-  {
-    path: 'tasks',
-    component: TasksComponent,
-    runGuardsAndResolvers: 'always',
-    resolve: {
-      userTasks: resolveUserTasks,
-    },
-  },
-  {
-    path: 'tasks/new',
-    component: NewTaskComponent,
-    canDeactivate: [canLeaveEditPage],
+    providers: [TasksService],
+    children: [
+      {
+        path: '',
+        redirectTo: 'tasks',
+        pathMatch: 'full',
+      },
+      {
+        path: 'tasks',
+        component: TasksComponent,
+        runGuardsAndResolvers: 'always',
+        resolve: {
+          userTasks: resolveUserTasks,
+        },
+      },
+      {
+        path: 'tasks/new',
+        component: NewTaskComponent,
+        canDeactivate: [canLeaveEditPage],
+      },
+    ],
   },
 ];
